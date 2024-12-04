5132fe8eea8fd1ba79ea09478b5700b1dca8f2d0
rogergheser <w100roger@gmail.com>
2024-12-04 08:14:59 +0000

    .

--- sam_pipeline.py
+++ sam_pipeline.py
@@ -2,14 +2,13 @@ import numpy as np
 import torch
 import matplotlib.pyplot as plt
 import cv2
+import time
 from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
 
 def show_anns(anns):
     if len(anns) == 0:
         return
     sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
-    ax = plt.gca()
-    ax.set_autoscale_on(False)
 
     img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
     img[:,:,3] = 0
@@ -17,18 +16,13 @@ def show_anns(anns):
         m = ann['segmentation']
         color_mask = np.concatenate([np.random.random(3), [0.35]])
         img[m] = color_mask
-    ax.imshow(img)
-
+    
+    cv2.imwrite('images/mask.jpg', img)
+    
 ### arguments
 
-device = 'mps'
-sam_checkpoint = ''
-image = cv2.imread('data/dog.jpg')
-model_type = 'vit_h'
 
-
-
-def pipeline():
+def pipeline(device, sam_checkpoint, image, model_type):
     sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
     sam.to(device=device)
 
@@ -53,9 +47,27 @@ def pipeline():
     # 'point_coords' : the sampled input point that generated this mask
     # 'stability_score' : an additional measure of mask quality
     # 'crop_box' : the crop of the image used to generate this mask in XYWH format
-
     plt.figure(figsize=(20,20))
     plt.imshow(image)
     show_anns(masks)
     plt.axis('off')
-    plt.show() 
+
+    return masks
+
+if __name__ == '__main__':
+    
+    device = 'cuda'
+    sam_checkpoint = './checkpoints/sam_vit_h_4b8939.pth'
+    image = cv2.imread('images/dog.jpg')
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    plt.figure(figsize=(20,20))
+    plt.imshow(image)
+    plt.axis('off')
+    plt.show()
+    time.sleep(2)
+    exit(1)
+    model_type = 'vit_h'
+
+    masks = pipeline(device, sam_checkpoint, image, model_type)
+    print('done')
